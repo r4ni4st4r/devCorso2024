c30101478391bbad1ba444575c099b812df626bf
r4ni4st4r <francesco.basevi@gmail.com>
2024-07-09 14:00:18 +0200

    Aggiunto esercitazioni Fizz Buzz

---
+++ fizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int i = 1;
+
+while(i < 101) {
+    
+    if(i%5 == 0 && i%3 == 0)
+        Console.WriteLine($"{i} --> Fizz Buzz"); 
+    else if(i%3 == 0)    
+        Console.WriteLine($"{i} --> Fizz");
+    else if(i%5 == 0)
+        Console.WriteLine($"{i} --> Buzz");
+    else
+        Console.WriteLine($"{i}"); 
+    
+    i++;
+}
---
+++ fizzBuzzPro/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Random number = new Random();
+int i = 1;
+int j;
+
+while(i < 101) {
+    
+    j = number.Next(1, 101);
+
+    if(j%5 == 0 && j%3 == 0)
+        Console.WriteLine($"{j} --> Fizz Buzz"); 
+    else if(j%3 == 0)    
+        Console.WriteLine($"{j} --> Fizz");
+    else if(j%5 == 0)
+        Console.WriteLine($"{j} --> Buzz");
+    else
+        Console.WriteLine($"{j}"); 
+    
+    i++;
+}
