c25647d350a34255306b888de8af3a5890c2e301
r4ni4st4r <francesco.basevi@gmail.com>
2024-07-15 01:04:37 +0200

    Lavoro di domenica a casa

--- 53-diceGameProMermaidSpectre/Program.cs
+++ 53-diceGameProMermaidSpectre/Program.cs
@@ -1,18 +1,91 @@
-﻿using Spectre.Console;
+﻿using System.Runtime.Serialization.Formatters;
+using Spectre.Console;
 
+Table topTable = new Table();
 int puntiUmano = 50; // Entrambi i giocatori iniziano con 100 punti
 int puntiPc = 50;
 
 Random random = new Random();
+/*
+List<string>  OrderList(List<string> listToOrder){
+    List<string> orderedList = new List<string>();
+    int bigger = 0;
+    int id=150; 
 
-void SalvaStampaPunteggi(bool umanOrPc, int deltaPunti){ //uman win = TRUE
-    int maxLines =10;
+    for(int i=0; i<listToOrder.Count(); i++){
+        for(int j=0; j<listToOrder.Count(); j++){
+            if (i!=j){
+                if(Convert.ToInt32(listToOrder[i].Split(',').First())>=Convert.ToInt32(listToOrder[j].Split(',').First())){
+                    bigger = Convert.ToInt32(listToOrder[i].Split(',').First());
+                }
+            }
+        }
+
+        if(bigger != 150){
+            orderedList.Add(listToOrder[i]);
+            listToOrder.Remove(listToOrder[i]);
+        }
+    }
+    return orderedList;
+}*/
+
+
+void SalvaStampaPunteggi(bool human, int deltaPunti){ //uman win = TRUE
     string path=@"topTen.txt";
-    string[] lines = new string[maxLines];
-    lines = File.ReadAllLines(path);
+    //Dictionary<int, (int,string)> topTenDictionary = new Dictionary<int, (int,string)>();
+    List<string> topTenList = new List<string>();  
+    /*
+    int index = 1;
+
+    foreach (string line in File.ReadAllLines(path)){
+        if(index<11){
+            if (line.Contains(","))
+                topTenList.Add(line);
+                //topTenDictionary.Add(index, (Convert.ToInt32(line.Split(',').First()), line.Split(',').Last()));
+        }
+        index++; 
+    }*/
+
+    using (StreamReader sr = new StreamReader(path)){
+        string line;
+        while ((line = sr.ReadLine()) != null){
+            topTenList.Add(line);
+        }
+    }
+
+    if(human)
+        topTenList.Add(deltaPunti.ToString()+","+"human");
+        //topTenDictionary.Add(11,(deltaPunti,"human"));
+    else
+        topTenList.Add(deltaPunti.ToString()+","+"cpu");
+        //topTenDictionary.Add(11,(deltaPunti,"cpu"));
+
+    int index=1;
+
+    //List<string> orderedList = OrderList(topTenList);
+    //topTenList.Sort();
+    //Console.WriteLine(topTenList.Count);
+    
+    for(int i=topTenList.Count-1;i>=0;i--){
+        if(index<=10){
+            if(index == 1){
+                using (StreamWriter sw = File.CreateText(path)){
+                    sw.WriteLine(topTenList[i]);
+                }	
+            }else{
+                using (StreamWriter sw = File.AppendText(path)){
+                    sw.WriteLine(topTenList[i]);
+                }	
+            }
+        }else
+            break;
+        index++;
+    }
+
+
     Table topTable = new Table();
     
-
+/*
     for(int i=0; i<lines.Length && i<maxLines; i++){
         
         var result = lines[i].Substring(lines[i].Length - 3).Trim();
@@ -33,16 +106,21 @@ void SalvaStampaPunteggi(bool umanOrPc, int deltaPunti){ //uman win = TRUE
             }
         }
         
+        
     }
-    
-    topTable.AddColumn("TOP 10 MIGLIORI VITTORIE");
+    */
+    topTable.AddColumn("ULTIME 10 PARTITE");
 
-    foreach(string line in lines){
-        File.AppendAllText(path, line + "\n");
-        topTable.AddRow(line);
+    for(int i=topTenList.Count-1;i>=0;i--){
+        if(topTenList[i].Split(',').Last()=="human")
+            topTable.AddRow("Ha vinto il giocatore con " + topTenList[i].Split(',').First()+ " di scarto");
+        else
+            topTable.AddRow("Ha vinto il computer con " + topTenList[i].Split(',').First()+ " di scarto");
     }
 
     AnsiConsole.Write(topTable);
+    
+    //Console.WriteLine(Convert.ToInt32(topTenList[0].Split(',').First()));
     
 }
 
@@ -52,17 +130,6 @@ int sommaUmano;
 int dado1Pc;
 int dado2Pc;
 int sommaPc;
-
-/*
-
-var table = new Table();
-
-table.AddColumn("Azione");
-table.AddColumn("Punti Umano");
-table.AddColumn("Punti PC");
-table.AddColumn("Situazione");
-
-*/
 
 while (puntiUmano > 0 && puntiPc > 0){
 
