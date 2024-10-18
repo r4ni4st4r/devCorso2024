942ab49eccf82a379c9fcebd8f76a7b4049328df
r4ni4st4r <francesco.basevi@gmail.com>
2024-10-18 17:54:55 +0200

    client server

---
+++ ChatApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+class Program{
+    static void Main(string[] args){
+        Client client = new Client();
+        Console.Write("Insert server IP address: ");
+        string serverIp = Console.ReadLine();
+        client.StartClient(serverIp, 3000);      
+    }
+
+    class Client{
+        public void StartClient(string serverIp, int port){
+            using(var stream = new TcpClient(serverIp,port).GetStream()){
+                Console.WriteLine("Connected to the server!");
+                string messageToSend = Console.ReadLine();
+                while(!string.IsNullOrEmpty(messageToSend)){
+                    byte[] buffer = Encoding.ASCII.GetBytes(messageToSend);
+                    stream.Write(buffer, 0, buffer.Length);
+                    messageToSend = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ ChatAppServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program{
+    static void Main(string[] args){
+        Server server = new Server();
+        server.StartServer(3000);      
+    }
+
+    class Server{
+        private TcpListener _tcpListener;
+        public void StartServer(int port){
+            _tcpListener = new TcpListener(IPAddress.Any, port);
+            _tcpListener.Start();
+            Console.WriteLine("Server started on port: " + port);
+        
+            while(true){
+                TcpClient client = _tcpListener.AcceptTcpClient();
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+                clientThread.Start(client);
+            }
+        }
+        private void HandleClient(object obj){
+            TcpClient client = (TcpClient)obj;
+            byte[] buffer = new byte[1024];
+            NetworkStream stream = client.GetStream();
+            int byteCount;
+
+            while((byteCount = stream.Read(buffer, 0, buffer.Length))!=0){
+                string message = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                Console.WriteLine("Recived: " + message);
+                Broadcast(message);
+            }
+            
+            client.Close();
+        }
+        private void Broadcast(string message){
+        }
+    }
+}
